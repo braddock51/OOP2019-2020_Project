7dc10e4548e036be87ead56db78ebdd3c771135c
braddock51 <braddock511@gmail.com>
2019-12-21 19:52:49 +0100

    Some update 21.12

--- OOP2019-2020_Project/Characters/Melees/Melee.cs
+++ OOP2019-2020_Project/Characters/Melees/Melee.cs
@@ -1,9 +1,33 @@
-﻿
+﻿using Armors;
+using Characters;
+using Weapons;
 
 namespace Melees
 {
-    public abstract class Melee
+    public abstract class Melee : Character
     {
+        public const int DEFAULT_MELEE_ABILITY_POINTS = 10;
+        public const int DEFAULT_MELEE_HEALTH_POINTS = 100;
 
+        private Weapon swordWeapon;
+        private Armor armor;
+
+        public Weapon
+        { 
+            get
+            {
+                return this.swordWeapon;
+            }
+            protected set
+            {
+                this.swordWeapon = value;
+            }
+        }
+
+        public Melee(Sword sword, Armor armor)
+            : base(string.Empty, DEFAULT_MELEE_ABILITY_POINTS, DEFAULT_MELEE_HEALTH_POINTS)
+        {
+
+        }
     }
 }
---
+++ OOP2019-2020_Project/Weapons/SwordWeapon/LongSword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2019_2020_Project.Weapons.SwordWeapon
+{
+    class LongSword
+    {
+    }
+}
